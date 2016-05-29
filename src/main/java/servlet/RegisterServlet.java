5dd257e294306481a19615f1c0f6fb910f30645b
tbpwang <tbpwang@gmail.com>
2016-05-29 16:29:33 +0800

    initial

--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
+import util.DB;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  * Created by Administrator on 2016/5/29.
@@ -14,7 +19,11 @@ import java.io.IOException;
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        Connection connection = DB.getConnection();
+        String sql = "";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
     }
 
     @Override
